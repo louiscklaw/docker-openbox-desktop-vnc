67e2af40c1ba152f9c671e3e25a5cbf8c7daaed2
Louis Law <louiscklaw@users.noreply.github.com>
2023-07-20 11:48:52 +0800

    update,

---
+++ test.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# 01 build openbox as base
+cd dockerfiles/01_openbox
+  docker build . -t openbox-android
+cd -
+
+# 02 build android emulator
+cd dockerfiles/02_android
+  docker build . -t openbox-android
+cd -
+
+# 03 build appium
+# cd dockerfiles/03_appium
+#   docker build . -t openbox-android
+# cd -
+
+# finialize docker
+cd dockerfiles
+  docker build . -t openbox-android
+cd -
+
+
+# docker build . -t logickee/docker_android
+# docker build -f ./Dockerfile.android . -t openbox-android
+
+docker run --rm \
+  --privileged \
+  --device /dev/kvm \
+  -v ./share:/share \
+  -p 15900:5900 \
+  --name logickee_docker_android openbox-android
